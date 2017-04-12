2e9f69ec0ca22f734ce3a450302cc1fc20634b5d
Rachel Munoz <rachelumunoz@gmail.com>
2017-04-12 16:19:21 -0700

    Refactors to constant lookup of cat instead of iterating through by adding an id to each cat and data set attribute

--- public/app.js
+++ public/app.js
@@ -5,22 +5,24 @@ window.onload = function(){
   let increaseCount = function(e){
     if (e.target.nodeName === 'IMG'){
 
-      let catName = e.target.parentElement.dataset.name;
-      let cat = cats.filter(cat => cat.name === catName ); //lot of searching
-      cat[0].clickCount = cat[0].clickCount + 1;
+      let catId = e.target.parentElement.dataset.id;
+      let cat = cats[catId]; 
+      cat.clickCount = cat.clickCount + 1;
 
-      e.target.nextElementSibling.firstElementChild.innerText = ` ${cat[0].clickCount}`;
+      e.target.nextElementSibling.firstElementChild.innerText = ` ${cat.clickCount}`;
     }
   }
   
   let cat1 = {
-    name: 'CherryPie', // can't have space in name with dataset attribute
+    id: 0,
+    name: 'Cherry Pie', 
     url: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
     clickCount: 0
   };
 
   let cat2 = {
-    name: 'WaterBottle', 
+    id: 1,
+    name: 'Water Bottle', 
     url: 'https://www.pets4homes.co.uk/images/articles/1646/large/kitten-emergencies-signs-to-look-out-for-537479947ec1c.jpg',
     clickCount: 0
   };
@@ -29,7 +31,7 @@ window.onload = function(){
 
   for (let i=0; i< cats.length; i++){
     let cat = ` 
-      <div class="cat" data-name=${cats[i].name}>
+      <div class="cat" data-id=${cats[i].id}>
           <img class="cat__image" src=${cats[i].url} alt="cute cat"/>
           <div class="cat__text">${cats[i].name} Clicks:<span class="cat__text--number"> ${cats[i].clickCount}</span></div>
       </div>`
